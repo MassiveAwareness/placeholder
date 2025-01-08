d1fa01a4d199cd82a18cb802ed38b5a2e4843907
Dave Summer <nyaridavid345@gmail.com>
2025-01-08 15:02:10 +0100

    New production rates for CivilWars

---
+++ newProdRates.js
@@ -0,0 +1,31 @@
+// New production rates
+
+let productionLevels = {
+    farms: {
+        1: 30,      // Increased 300%
+        2: 50,      // Increased 167%
+        3: 70,      // Increased 200%
+        4: 100,     // Increased 200%
+        5: 125,     // Increased 179%
+        6: 175,     // Increased 184%
+        7: 250      // Increased 208%
+    },
+    mines: {
+        1: 50,      // Increased 333%
+        2: 80,      // Increased 267%
+        3: 120,     // Increased 240%
+        4: 180,     // Increased 240%
+        5: 250,     // Increased 250%
+        6: 350,     // Increased 280%
+        7: 500      // Increased 333%
+    },
+    houses: {
+        1: 10,      // Increased 200%
+        2: 20,      // Increased 200%
+        3: 30,      // Increased 200%
+        4: 40,      // Increased 200%
+        5: 50,      // Increased 200%
+        6: 60,      // Increased 200%
+        7: 70       // Increased 200%
+    }
+};
