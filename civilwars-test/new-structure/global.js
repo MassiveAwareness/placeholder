e7b7724c62de7fffd4f1be14c0f5601da41730ea
MassiveAwareness <massive.awareness.ofc@gmail.com>
2025-06-25 14:37:22 +0200

    Delete civilwars-test directory

--- civilwars-test/new-structure/global.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const isLoggedIn = localStorage.getItem('loggedInUser');
-
-    if(!isLoggedIn) window.location.href = './auth/login.html';
-});
